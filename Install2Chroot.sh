1aec95e0867dd251cf8e638f63255402d14cab4c
Thomas H Jones II <github@xanthia.com>
2014-09-26 23:33:24 +0000

    Updated RPM installation list to as closely mimic the "real" ICGC as possible.

--- Install2Chroot.sh
+++ Install2Chroot.sh
@@ -3,15 +3,13 @@
 # Install minimal RPM-set into chroot
 #
 #####################################
-CHROOT=${CHROOT:-/mnt/ec2-root}"
+CHROOT="${CHROOT:-/mnt/ec2-root}"
 CONFROOT=`dirname $0`
 
 # Install main RPM-groups
-yum -c ${CONFROOT}/yum-build.conf --installroot=${CHROOT} -y groupinstall Base \
-Core Core
-
-# Install additional individual RPMs (dependencies pulled in automagically
-yum -c ${CONFROOT}/yum-build.conf --installroot=${CHROOT} -y install kernel \
+yum -c ${CONFROOT}/yum-build.conf --installroot=${CHROOT} install -y @Base \
+@Core -- \
+kernel \
 acpid \
 aide \
 audit \
@@ -38,21 +36,17 @@ vim-enhanced \
 wget \
 which \
 yum-cron \
-yum-utils 
-
-
-# Remove unwanted RPMs installed by the RPM groups
-yum -c ${CONFROOT}/yum-build.conf --installroot=${CHROOT} -y erase \
-abrt \
-abrt-addon-ccpp \
-abrt-addon-kerneloops \
-abrt-addon-python \
-abrt-cli \
-abrt-libs \
-gcc-gfortran \
-libvirt-client \
-libvirt-devel \
-libvirt-java \
-libvirt-java-devel \
-nc \
-sendmail
+yum-utils \
+-abrt \
+-abrt-addon-ccpp \
+-abrt-addon-kerneloops \
+-abrt-addon-python \
+-abrt-cli \
+-abrt-libs \
+-gcc-gfortran \
+-libvirt-client \
+-libvirt-devel \
+-libvirt-java \
+-libvirt-java-devel \
+-nc \
+-sendmail 
