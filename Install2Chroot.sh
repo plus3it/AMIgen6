4ef2c5340a0716f1cf53be1b2e588c6f141eba5f
Thomas H Jones II <github@xanthia.com>
2014-09-26 04:12:49 +0000

    Typos...

--- Install2Chroot.sh
+++ Install2Chroot.sh
@@ -5,4 +5,4 @@
 #####################################
 
 yum -c /opt/ec2/yum/yum-xen.conf --installroot=/mnt/ec2-root/ -y groupinstall core server-policy workstation-policy
-yum -c /opt/ec2/yum/yum-xen.conf --installroot=/mnt/ec2-root/ -y install kernel grub e2fsprogs lvm2 wget openssh-clients openssh-server dhclient selinux-policy selinux-policy-targeted vi
+yum -c /opt/ec2/yum/yum-xen.conf --installroot=/mnt/ec2-root/ -y install kernel grub e2fsprogs lvm2 wget openssh-clients openssh-server dhclient selinux-policy selinux-policy-targeted vim-enhanced
