1685146e8f407e65904e481f05489947095749e0
Thomas H Jones II <github@xanthia.com>
2014-10-03 21:02:54 +0000

    Created install content

--- ChrootBuild3.sh
+++ ChrootBuild3.sh
@@ -1,45 +1,50 @@
 #!/bin/bash
 #
-# (Re)Install RPMs that failed to be installed as part of the prior
-#   RPM-handling phases
+# Add back RPMs that were removed by dependency-tracking in prior step
 #
-######################################################################
+###########################################################################
 CHROOT="${CHROOT:-/mnt/ec2-root}"
 CONFROOT=`dirname $0`
+DATE=`/bin/date "+%Y%m%d-%H%M%S"`
+ADDLST="${CONFROOT}/install.lst"
+YUMINVOKE="yum -c ${CONFROOT}/yum-build.conf --nogpgcheck --installroot=${CHROOT}"
+YUMLOG="/var/tmp/install3_log.${DATE}"
 
-# Install main RPM-groups
-yum -c ${CONFROOT}/yum-build.conf --nogpgcheck --installroot=${CHROOT} install -y \
-cups \
-cups-libs \
-device-mapper-persistent-data \
-dracut \
-dracut-kernel \
-foomatic \
-foomatic-db \
-foomatic-db-ppds \
-ghostscript \
-gtk2 \
-jasper-libs \
-libdrm \
-libjpeg-turbo \
-libmng \
-libpciaccess \
-libtiff \
-lvm2 \
-m2crypto \
-mesa-dri1-drivers \
-mesa-dri-drivers \
-mesa-dri-filesystem \
-mesa-libGL \
-mesa-libGLU \
-phonon-backend-gstreamer \
-plymouth \
-poppler \
-poppler-utils \
-python-ldap \
-qt3 \
-qt-x11 \
-redhat-lsb-compat \
-redhat-lsb-core \
-redhat-lsb-graphics \
-redhat-lsb-printing 
+logit() {
+   if [ $1 -ne 0 ]
+   then
+      printf -- "----------\nNote:%s\n----------\n" "$2" | tee -a ${YUMLOG}
+   else
+      echo "$2" | tee -a ${YUMLOG}
+   fi
+}
+
+# Grab our current RPM-manifest
+(chroot ${CHROOT} rpm -qa) > ${YUMLOG}.start
+RPMCNT=`wc -l ${YUMLOG}.start | cut -d " " -f 1`
+
+logit 0 "Started with ${RPMCNT} RPMs"
+
+# Iterate install list and add one at a time 
+for RPMCHK in `cat ${ADDLST}`
+do
+   (chroot $CHROOT rpm -q ${RPMCHK}) > /dev/null 2>&1
+   if [ $? -eq 0 ]
+   then
+      logit "0" "Package ${RPMCHK} already installed. Skipping"
+   else
+      logit "0" "Attempting to install package ${RPMCHK}."
+      ${YUMINVOKE} -q -y install ${RPMCHK} 2>&1 | \
+         sed '{
+            /listed more than once/d
+            /transaction-done/d
+         }' | tee -a ${YUMLOG}
+   fi
+      
+done
+
+# Grab our final RPM-manifest
+(chroot ${CHROOT} rpm -qa) > ${YUMLOG}.end
+RPMCNT=`wc -l ${YUMLOG}.end | cut -d " " -f 1`
+
+logit 0 "Finished with ${RPMCNT} RPMs"
