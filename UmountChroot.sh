2b7269165f404c4d89d9e2041a38e72010c123f3
Thomas H Jones II <github@xanthia.com>
2015-02-25 14:34:11 +0000

    Updated UmountChroot.sh * Changed mount-enumeration technique to be more flexible

--- UmountChroot.sh
+++ UmountChroot.sh
@@ -4,24 +4,31 @@
 #
 ########################################
 CHROOT=${CHROOT:-/mnt/ec2-root}
+RESULT=0
 
 sync ; sync ;sync
 
-# Kill loopbacks
-umount ${CHROOT}/sys
-umount ${CHROOT}/dev/shm
-umount ${CHROOT}/dev/pts
-umount ${CHROOT}/proc
-
-# Kill the rest of the chroot
-umount ${CHROOT}/home/
-umount ${CHROOT}/opt/
-umount ${CHROOT}/var/log/audit/
-umount ${CHROOT}/var
-umount ${CHROOT}/boot
-umount ${CHROOT}/
+for MOUNT in `mount | awk '/mnt\/ec2-root/{ print $3}' | sed '1!G;h;$!d'`
+do
+   printf "Attempting to umount ${MOUNT}... "
+   umount ${MOUNT}
+   if [ $? -eq 0 ]
+   then
+      echo "success!"
+   else
+      echo "FAILED!"
+      RESULT=1
+   fi
+done
 
 # Deactivate the chroot VG
 vgchange -a n VolGroup00
 
-echo "Now prepped for EBS snapshot"
+if [ ${RESULT} -eq 0 ]
+then
+   echo "Now prepped for EBS snapshot"
+else
+   echo "Some prep steps failed. Manually ensur that:"
+   echo "* All chroot-mounts offlined"
+   echo "* All LVM objects deconfigured"
+fi
