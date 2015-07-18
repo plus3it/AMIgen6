a160db57f46012928a44f38295ee148ccc2b2737
Thomas H Jones II <github@xanthia.com>
2015-07-18 00:31:35 +0000

    Initial check-in of PullSRPMs.sh

---
+++ AWSpkgs/PullSRPMs.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#
+# Script to spin up an instance of the latest-available Amazon
+# Linux instance from which to extract the various AWS CLI
+# utilities and modules
+#
+#################################################################
+AWSBIN="/opt/aws/bin/aws"
+export AWS_DEFAULT_REGION=$(curl -s \
+   http://169.254.169.254/latest/dynamic/instance-identity/document | \
+   awk -F":" '/"region"/{ print $2 }' | sed -e 's/^ "//' -e 's/".*$//')
+
+echo "Determining latest-available Amazon Linux AMI-ID..."
+TARGAMI=$(${AWSBIN} ec2 describe-images --owner amazon --filters \
+   "Name=image-type,Values=machine" "Name=architecture,Values=x86_64" \
+   "Name=root-device-type,Values=ebs" "Name=name,Values=amzn-ami-hvm-*" \
+   --query 'Images[].{ID:ImageId,NAME:Name,DATE:CreationDate}' \
+   --output text | sort -n | sed -n '$p' | awk '{print $2}')
+
+echo $TARGAMI
