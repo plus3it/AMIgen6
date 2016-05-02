71f69afd705c6008b2d0d3792dee0a2500c6f458
Thomas H Jones II <github@xanthia.com>
2016-05-02 15:34:28 -0400

    Add selectable private repos (Fixes #60) * Added dump of error output to /dev/null. Closes #58 if logic is right.

--- ChrootBuild.sh
+++ ChrootBuild.sh
@@ -3,8 +3,9 @@
 # Install minimal RPM-set into chroot
 #
 #####################################
+PROGNAME=$(basename "$0")
 CHROOT="${CHROOT:-/mnt/ec2-root}"
-CONFROOT=`dirname $0`
+CONFROOT=$(dirname $0)
 
 function PrepChroot() {
    local DISABLEREPOS="*media*,*epel*,C*-*"
@@ -16,13 +17,65 @@ function PrepChroot() {
 
    yumdownloader --destdir=/tmp $(rpm -qf /etc/redhat-release)
    yumdownloader --destdir=/tmp $(rpm --qf '%{name}\n' \
-      -qf /etc/yum.repos.d/* | sort -u)
+      -qf /etc/yum.repos.d/* 2> /dev/null | sort -u)
    rpm --root ${CHROOT} --initdb
    rpm --root ${CHROOT} -ivh --nodeps /tmp/*.rpm
    yum --enablerepo=* --disablerepo=${DISABLEREPOS} --installroot=${CHROOT} \
       -y reinstall $(rpm --qf '%{name}\n' -qf /etc/yum.repos.d/* | sort -u)
+
+   # if alt-repo defined, disable everything, then install alt-repo
+   if [[ ! -z ${REPORPM+xxx} ]]
+   then
+      for FILE in ${CHROOT}/etc/yum.repos.d/*.repo
+      do
+         sed -i '{
+	    /^\[/{N
+	       s/\n/&enabled=0\n/
+            }
+	    /^enabled=1/d
+         }' "${FILE}"
+      done
+      rpm --root ${CHROOT} -ivh --nodeps "${REPORPM}"
+   fi
 }
 
+
+######################
+## Main program flow
+######################
+
+# See if we'e passed any valid flags
+OPTIONBUFR=$(getopt -o r: --long repouri: -n ${PROGNAME} -- "$@")
+eval set -- "${OPTIONBUFR}"
+
+while [[ true ]]
+do
+   case "$1" in
+      -r|--repouri)
+         case "$2" in
+	    "")
+	       echo "Error: option required but not specified" > /dev/stderr
+	       shift 2;
+	       exit 1
+	       ;;
+	    *)
+	       REPORPM=${2}
+	       shift 2;
+	       ;;
+	 esac
+	 ;;
+      --)
+         shift
+	 break
+	 ;;
+      *)
+         echo "Internal error!" > /dev/stderr
+	 exit 1
+	 ;;
+   esac
+done
+
+# Stage useable repo-defs into $CHROOT/etc/yum.repos.d
 PrepChroot
 
 # Install main RPM-groups
