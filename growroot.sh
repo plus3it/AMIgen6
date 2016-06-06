12abc35e2f499be1bee58598fa9dcf1dad7c877b
Thomas H Jones II <github@xanthia.com>
2016-06-06 17:46:55 -0400

    Fix message logged when '/' hosted on LVM2

--- growroot.sh
+++ growroot.sh
@@ -21,13 +21,12 @@ _growroot() {
 	# Compute root-device
 	if [ -z "${root##*mapper*}" ]
 	then
-##		_info "${root} is hosted on an LVM2 volume"
 		set -- "${root##*mapper/}"
 		VOLGRP=${1%-*}
 		ROOTVOL=${1#*-}
 		rootdev=$(readlink -f $(pvs --noheadings | awk '/'${VOLGRP}'/{print $1}'))
-		_info "Setting \$rootdev to ${rootdev} is hosted on an LVM2 volume"
-        else
+		_info "'/' is hosted on an LVM2 volume: setting \$rootdev to ${rootdev}"
+	else
 		# Remove 'block:' prefix and find the root device
 		rootdev=$(readlink -f "${root#block:}")
 	fi
